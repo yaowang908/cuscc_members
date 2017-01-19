33df139e4a208d9c00e736231b8694477e574fe6
Yao Wang <yao422321065@yahoo.com>
2017-01-19 18:11:04 -0500

    add last/next button

--- plugin_js/cuscc_members_admin_page_js.js
+++ plugin_js/cuscc_members_admin_page_js.js
@@ -1,6 +1,6 @@
 
 (function($){
-    
+   var displayedMemberAmount = 4;
 //js select display mode
     $(document).ready(function(){
           $("label").click(function(){
@@ -110,7 +110,9 @@
                             $items.find('.clear').remove();
                              $items.append('<div class="members_slide_show_item_container">             <img src="'+resultToArray+'"class="members_slide_show_item" data-index="'+$itemIndex+'"/><div class="members_slide_show_item_buttons hide">                        <button class="change_slide_show_item left">Change Title</button><button class="delete_current_slide_show_item left">delete</button></div></div>');
                             $items.append('<div class="clear"></div>');
-                           
+                            displayedMemberAmount = $itemIndex;
+                            slideContainerWidth = $itemIndex * 210;
+                            $items.attr('width',slideContainerWidth);
                         
                         },
                         error: function (ErrorResponse) {
@@ -209,7 +211,60 @@
             
         });
     });
-    
+ 
+//last or next image
+    $(document).ready(function(){
+        var $container = $('.cm_display_1_slide_mode');
+        var $itemsContainer = $('#members_slide_show_container');
+        $container.append('<div class="pagination_btn last_item_btn">&lt;</div>');
+        $container.append('<div class="pagination_btn next_item_btn">&gt;</div>');
+        $('head').append('<style>.pagination_btn{width:40px;height:40px;position:absolute;top:50%;margin-top:-20px;text-align:center;font-size:40px;line-height:40px;background-color:rgba(255,255,255,0.7);cursor:pointer;}.last_item_btn{left:0px;}.next_item_btn{right:0px;}</style>');
+        var $lastBtn = $('.cm_display_1_slide_mode .last_item_btn');
+        var $nextBtn = $('.cm_display_1_slide_mode .next_item_btn');
+        
+        //reach the end
+                
+        var itemsContainerPositionCount = 0;
+        var $lastBtnState =0;
+         var $nextBtnState = 1;
+        $lastBtn.on('click',function(){
+            
+            if(itemsContainerPositionCount > 0 ){
+                $itemsContainer.animate({left: "+=210"},1000,function(){
+                    $itemsContainer.css('margin-left','+= 210');
+                });
+                itemsContainerPositionCount -= 1;
+            }else if(itemsContainerPositionCount <= 0 && $lastBtnState){
+                //alert('$lastBtn the end ');
+                $itemsContainer.animate({left: "+=40"},1000,function(){
+                    $itemsContainer.css('margin-left','+= 40');
+                });
+                $lastBtnState = 0;
+            }else{
+                alert('lastBtn the end');
+            }
+        });
+        $nextBtn.on('click',function(){
+           
+            if((displayedMemberAmount-itemsContainerPositionCount)>=4){
+                $itemsContainer.animate({left: "-=210"},1000,function(){
+                     $itemsContainer.css('margin-left','-= 210');
+                });
+                itemsContainerPositionCount +=1;
+            }else if((displayedMemberAmount-itemsContainerPositionCount)<4 && $nextBtnState){
+                //alert('$nextBtn the end');
+                $itemsContainer.animate({left: "-=40"},1000,function(){
+                     $itemsContainer.css('margin-left','-= 40');
+                });
+                $nextBtnState = 0;
+                $lastBtnState = 1;
+            }else{
+                alert('nextBtn the end');
+            }
+        });
+    });
+
+
 })(jQuery);
 
 
