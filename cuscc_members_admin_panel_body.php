232aeb0307ac8b6d1948ddbfaf0ce7dd6d96cdc7
Yao Wang <yao422321065@yahoo.com>
2017-01-29 16:31:20 -0500

    remove strategypatners module

--- cuscc_members_admin_panel_body.php
+++ cuscc_members_admin_panel_body.php
@@ -2,19 +2,6 @@
        
 ?>
 
-<div class='cm_display_item' id='cm_display_1'>
-  <div class='cm_select_display_mode'>
-    <form action="#" method="post" >
-        <input type='radio' id='cm_display_1_slide_mode' checked='checked'name='cm_display_1_mode' value='slide mode'/>
-        <label name='cm_display_1_slide_mode' class='checked' for='cm_display_1_slide_mode'>slide mode</label>
-        <input type='radio' id='cm_display_1_grid_mode' name='cm_display_1_mode' value='grid mode'/>
-        <label name='cm_display_1_grid_mode' class='' for='cm_display_1_grid_mode'>grid mode</label>
-     </form> 
-  </div>
-  <div class='cm_upload_holder'>
-    <p>upload images</p>
-    <input type='text' id='cm_display_1_add_link' name='cm_display_1_add_link' value='add image link'/>
-  </div>
   <div class='cm_uploaded_items cm_display_1_slide_mode show'>
      slide mode looks like this
 <?php 
@@ -52,8 +39,4 @@
           <div class="clear"></div>
       </div>
   </div>
-    
-  <div class='cm_uploaded_items cm_display_1_grid_mode hide'>
-    grid mode looks like this
-  </div>
-</div>
+  
